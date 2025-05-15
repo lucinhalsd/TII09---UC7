8dfaf1ccf97883e780d3533f038575dea5834e37
lucimara <lucinha.lsd06@gmail.com>
2025-05-14 22:14:39 -0300

    aula 14/5/25

--- 05/pizzaria/Pizza.php
+++ 05/pizzaria/Pizza.php
@@ -1,12 +1,14 @@
 <?php
 
-class Pizza {
-    private int $id;
+class Pizza
+{
+    private  $id;
     private string $sabor;
     private string $tamanho;
     private float $preco;
 
-    public function __construct($sabor, $tamanho, $preco, $id) {
+    public function __construct($id, $sabor, $tamanho, $preco)
+    {
         $this->id = $id;
         $this->sabor = $sabor;
         $this->tamanho = $tamanho;
@@ -14,28 +16,32 @@ class Pizza {
     }
 
     public function getId(): float
-     { return $this->id; }
-
-    public function getSabor(): float 
-    { return $this->sabor; }
-
-    public function getTamanho(): float
-     { return $this->tamanho; }
-     
+    {
+        return $this->id;
+    }
+    public function getSabor(): string
+    {
+        return $this->sabor;
+    }
+    public function getTamanho(): string
+    {
+        return $this->tamanho;
+    }
     public function getPreco(): float
-     { return $this->preco; }
+    {
+        return $this->preco;
+    }
 
-
-   
-    public function setPreco(float $novoPreco): void {
-        if($novoPreco > 0) {
+    public function setPreco(float $novoPreco): void
+    {
+        if ($novoPreco > 0) {
             $this->preco = $novoPreco;
         }
     }
 
     public function __toString()
     {
-        return "Pizza de $this->sabor e preco $this->preco";
+        return "Pizza de $this->sabor e preco $this->preco <br>";
     }
 }
    
