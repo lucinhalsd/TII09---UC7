e4e7ee423b7549886771274b422325ca43e6a68c
lucimara <lucinha.lsd06@gmail.com>
2025-06-25 22:15:57 -0300

    aula 12  24/6/25

---
+++ 12/model/Fornecedor.php
@@ -0,0 +1,25 @@
+<?php
+
+class Fornecedor
+{
+    private ?int $id;
+    private string $nome;
+    private ?string $cnpj;
+    private ?string $contato;
+    
+    public function __construct($id, $nome, $cnpj, $contato)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->cnpj = $cnpj;
+        $this->contato = $contato;
+    }
+    
+    public function getId(): ?int { return $this->id;}
+    public function getNome(): string { return $this->nome;}
+    public function getCpnj(): ?string { return $this->cnpj;}
+    public function getContato(): ?string  { return $this->contato;}
+}
+
+        
+    
